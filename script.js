76ef944e85800ffe9908a75976744887921abefc
Mahendra <mahendra.cse.iiit@gmail.com>
2022-03-30 15:00:14 +0530

    js loosely typed dynamic typed

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-//default global execution environment is window or this at global level
-// anything not inside a function is in global environment
-var x = 1;
 
-function a(){
-    var y = 3;
-}
+var a;
+console.log(a);
 
-console.log(x);
-console.log(window.x);
-console.log(this.x);
-console.log(this.a);
-console.log(this == window);
+a = 10;
+console.log(a);
 
+a = "mahendra";
+console.log(a);
+
+a = true;
+console.log(a);
+
+a = 12.56;
+console.log(a);
+
+//never do this, because by default js engine assigns undefined if it is not assigned 
+a = undefined;
+console.log(a);
