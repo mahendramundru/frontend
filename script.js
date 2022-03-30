85ad4eb25ec146ea8f0aff7eb7cb6b5bd483ea61
Mahendra <mahendra.cse.iiit@gmail.com>
2022-03-30 14:25:37 +0530

    js variables in different scope

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-// JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of the code. Hoisting allows functions to be safely used in code before they are declared.
-console.log(square(10));
+//variable overriding in variable environment
+x = 1;
+
+a();
+b();
 
 console.log(x);
-function square(num){
-    return num*num;
-}
-var x = 10;
-var square2 = (n) => {
-    return n*n;
+
+function a(){
+    var x = 10;
+    console.log(x);
 }
 
-var square3 = function(m){
-    return m*m;
+function b(){
+    var x = 100;
+    console.log(x);
 }
 
-console.log(square2(20));
-console.log(square3(100));
+function c(){
+    console.log(x); //gives undefined
+    var x = 1000;
+}
