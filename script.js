6a09b675bbb8bbb1206770d19fd0ccfdf8775e25
Mahendra <mahendra.cse.iiit@gmail.com>
2022-03-31 17:09:00 +0530

    js setTimeout with args and comments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 for(var i=1; i<=10; i++){  // for each iteration it creates a separate i
-    setTimeout(function(i){
-        console.log(i); // for each of this inner function has a separate let i
+    setTimeout(function(x){ //here this x is an argument within the function scope
+        console.log(x); // for each of this inner function has a separate let i
         
-    }, i*1000, i);
+    }, i*1000, i); // this i value will be passed to that argument
 }
