d0d02ee3f83255fbb470d8a000992d6d2ded3e9c
Mahendra <mahendra.cse.iiit@gmail.com>
2022-03-31 16:56:00 +0530

    js setTimeout using let

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-var i = 1;
-var intervalId = setInterval(function(){
-    console.log(i);
-    i = i + 1;
-    if(i >10 ){
-        clearInterval(intervalId);
-    }
-}, 1000);
+for(let i=1; i<=10; i++){  // for each iteration it creates a separate i
+    setTimeout(function(){
+        console.log(i); // for each of this inner function has a separate let i
+    }, i*1000);
+}
