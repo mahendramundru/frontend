12a7fe249c0d0f850791aa2db6a803ed80a7f90b
Mahendra <mahendra.cse.iiit@gmail.com>
2022-03-31 16:47:49 +0530

    js setInterval

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-function f0(){ // it is a closure function as it encloses the inner function
-    var a0 = 0;
-    return f1();
-    function f1(){ // it is a closure function as it encloses the inner function
-        var a1 = 1;
-        // f2();
-        function f2(){
-            var a2 = 2;
-            console.log(a2);
-            console.log(a1);
-            console.log(a0);
-        }
-        return f2; // returning the function f2 it will be returned along with its environment and it would be able to access a0, a1, a2 
+var i = 1;
+var intervalId = setInterval(function(){
+    console.log(i);
+    i = i + 1;
+    if(i >10 ){
+        clearInterval(intervalId);
     }
-}
-var rf = f0();
-console.log(rf);
-rf(); //calling the returned function
-
-
-//not related to closure just testing some ideas
-var tf = function ff(){
-    console.log("hi");
-}
-tf();
-var tf2 = function (){
-    console.log("hi hi");
-}
-tf2();
+}, 1000);
